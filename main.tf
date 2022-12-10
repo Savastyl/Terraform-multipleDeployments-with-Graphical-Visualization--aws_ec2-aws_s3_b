25a20652a1f0e7894b66025194af8daba775d133
Savas <savastyludf@hotmail.com>
2022-12-10 02:05:06 -0500

    first commit

--- main.tf
+++ main.tf
@@ -15,11 +15,26 @@ resource "aws_instance" "tf-ec2" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
   tags = {
-    "Name" = "terraform"
-  }
-  count = 5
+    "Name" = "terraform535353-${count.index}"
+    
+    }
+  
+  count = 2
+ 
 }
+
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket345353"
+  bucket = "my-tf-test-bucket345353-${count.index}"
+  count = 2
  }
 
+output "ec2-public" {
+  value = aws_instance.tf-ec2[0].public_ip
+  
+}
+
+output "aws_s3_bucket" {
+  value = aws_s3_bucket.b[0].region
+  
+}
+ 
